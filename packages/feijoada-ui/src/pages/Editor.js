48912d58a58fbbd2d0b16d892b0c97ec4d136736
Gabriel Rodrigues <jipacoding@gmail.com>
2020-06-22 11:45:32 -0400

    feat(feijoada-ui): Init Editor Page
    Includes Header component

---
+++ packages/feijoada-ui/src/pages/Editor.js
@@ -0,0 +1,12 @@
+import * as React from "react";
+import Header from "../components/Header";
+
+const Editor = () => (
+  <div
+    className="tw-flex tw-flex-col tw-h-full tw-min-h-screen tw-bg-gray-900 tw-text-white"
+  >
+    <Header />
+  </div>
+);
+
+export default Editor;
