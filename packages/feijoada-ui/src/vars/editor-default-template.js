6ebd892d88ddcbc7a3f289c3285c654be6a1b550
Gabriel Rodrigues <jipacoding@gmail.com>
2020-06-22 15:07:17 -0400

    feat(feijoada-ui): Default Editor Template Doc

---
+++ packages/feijoada-ui/src/vars/editor-default-template.js
@@ -0,0 +1,17 @@
+const EDITOR_DEFAULT_TEMPLATE = {
+  description: "",
+  publisher: "admin",
+  model: {
+    sketch: {
+      width: 350,
+      height: 400,
+    },
+    staticImages: [],
+    fabricExported: {
+      version: "3.6.3",
+      objects: [],
+    },
+  },
+};
+
+export default EDITOR_DEFAULT_TEMPLATE;
