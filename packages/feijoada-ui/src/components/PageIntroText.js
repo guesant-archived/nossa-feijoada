d856a5b9bef0e62aeb35da5bd9b4ae6a7b4172da
Gabriel Rodrigues <jipacoding@gmail.com>
2020-06-22 10:41:41 -0400

    feat(feijoada-ui): Component PageIntroTextP

---
+++ packages/feijoada-ui/src/components/PageIntroText.js
@@ -0,0 +1,9 @@
+import * as React from "react";
+
+export const PageIntroTextP = ({ children }) => (
+  <p
+    className="tw-mt-0 sm:tw-mt-1 tw-text-center tw-text-white tw-text-sm sm:tw-text-base md:tw-text-base"
+  >
+    {children}
+  </p>
+);
