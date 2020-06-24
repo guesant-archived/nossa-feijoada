3b5e02b4391aa49520ffffdfea98df84bd894964
Gabriel Rodrigues <jipacoding@gmail.com>
2020-06-24 10:15:44 -0400

    feat(core): lib/buildObject

--- packages/core/src/lib/index.ts
+++ packages/core/src/lib/index.ts
@@ -1,3 +1,4 @@
+export { default as buildObject } from "./build-object";
 export { default as buildStatic } from "./build-static";
 export { default as imageFromURL } from "./image-from-url";
 export { default as randomID } from "./random-id";
