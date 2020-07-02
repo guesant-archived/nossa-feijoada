abb90a1f7b56bebb327d338978f8ccf984ac08cb
Gabriel Rodrigues <jipacoding@gmail.com>
2020-07-02 15:19:10 -0400

    feat(feijoada-ui): Home: HomeSourceImage
    Can edit `object.src` with `input{type: file}`

--- packages/feijoada-ui/src/components/HomeSource.js
+++ packages/feijoada-ui/src/components/HomeSource.js
@@ -1,9 +1,11 @@
 import * as React from "react";
+import HomeSourceImage from "./HomeSourceImage";
 import HomeSourceTextBox from "./HomeSourceTextBox";
 
 const HomeSource = ({ type, ...props }) =>
   React.createElement(
     {
+      image: HomeSourceImage,
       textbox: HomeSourceTextBox,
     }[type] || (() => <div></div>),
     { ...props },
