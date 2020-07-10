42393e3cff1f9fb90c8558218e301aefbda3b02a
Gabriel Rodrigues <jipacoding@gmail.com>
2020-07-10 09:16:12 -0400

    feat(feijoada-ui): Editor: Import local template

--- packages/feijoada-ui/src/components/HomeImportTemplate.js
+++ packages/feijoada-ui/src/components/HomeImportTemplate.js
@@ -4,29 +4,31 @@ import optimizeDoc from "../utils/optimize-doc";
 import blobAsText from "../utils/blob-as-text";
 import EDITOR_DEFAULT_TEMPLATE from "../vars/editor-default-template";
 
-const HomeImportTemplate = ({ onUpdateDoc }) => (
-  <Button>
-    <label className="tw-mb-0">
-      <span>Abrir Template</span>
-      <input
-        type="file"
-        className="tw-sr-only"
-        onChange={async ({
-          target: {
-            files: [file],
-          },
-        }) => {
-          if (!file) return;
-          try {
-            await onUpdateDoc(
-              await optimizeDoc(JSON.parse(await blobAsText(file))),
-            );
-          } catch (_) {
-            await onUpdateDoc(await optimizeDoc(EDITOR_DEFAULT_TEMPLATE));
-          }
-        }}
-      />
-    </label>
+const HomeImportTemplate = ({ buttonProps = {}, onUpdateDoc }) => (
+  <Button
+    {...buttonProps}
+    className={[buttonProps.className || "", "tw-mb-0"]}
+    as="label"
+  >
+    <span>Abrir Template</span>
+    <input
+      type="file"
+      className="tw-sr-only"
+      onChange={async ({
+        target: {
+          files: [file],
+        },
+      }) => {
+        if (!file) return;
+        try {
+          await onUpdateDoc(
+            await optimizeDoc(JSON.parse(await blobAsText(file))),
+          );
+        } catch (_) {
+          await onUpdateDoc(await optimizeDoc(EDITOR_DEFAULT_TEMPLATE));
+        }
+      }}
+    />
   </Button>
 );
 
