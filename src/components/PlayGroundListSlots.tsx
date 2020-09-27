060e02de8622166c555fda6d2f118c859ead1bb7
Gabriel Rodrigues <jipacoding@gmail.com>
2020-09-27 08:54:34 -0400

    feat: gap in PlayGroudListSlots

--- src/components/PlayGroundListSlots.tsx
+++ src/components/PlayGroundListSlots.tsx
@@ -9,7 +9,7 @@ export const PlayGroundListSlots = ({
 }: PlayGroundExtendProps) => {
   const { template } = extendProps;
   return (
-    <ul>
+    <ul style={{ display: "flex", flexDirection: "column", gap: 12 }}>
       {template.model.fabricExported.objects.map((object, idx, { length }) => {
         const slotProps = { ...extendProps, object, idx };
         return (
